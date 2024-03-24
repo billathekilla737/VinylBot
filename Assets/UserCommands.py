daaa8acacf35567fa2bcdeee1dd2615e12187a83
Zack <zacharygarris737@gmail.com>
2024-03-23 22:14:13 -0500

    Bed Time

--- Assets/UserCommands.py
+++ Assets/UserCommands.py
@@ -1,47 +1,51 @@
-import discord 
+import discord
 import json
 from discord.ext import commands
 
-# Initialize the bot
-bot = commands.Bot(command_prefix='!')
+class CommandCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.SearchFile = 'Assets/SearchParams.json'
 
-SearchFile = 'Assets/SearchParams.json'
-Search = set()
-SearchList = []
+    @commands.command()
+    async def add_to_list(self, ctx, *, item: str):
+        # Get the user ID and add the item to the search list
+        user_id = ctx.author.id
+        data_entry = {"user_id": user_id, "item": item}
 
-# ADD TO LIST FUNCTION
-@bot.command()
-async def add_to_list(ctx, *, item: str):
-    # Get the username of the user who sent the command
-    username = ctx.author.name
-    # Add the item and username to the Searchlist
-    SearchList.append((username, item))
-    # Add the item and username to the SearchFile
-    with open(SearchFile, 'a') as f:
-        f.write(username + ' ' + item + '\n')
-    # Send a confirmation message
-    await ctx.send(f"{item} added to the list along with your Discord username, {username}.")
+        # Append the new entry to the JSON file
+        with open(self.SearchFile, 'r+') as f:
+            try:
+                data = json.load(f)
+            except json.JSONDecodeError:
+                data = []
+            data.append(data_entry)
+            f.seek(0)
+            json.dump(data, f, indent=4)
 
-@bot.command()
-async def show_whole_list(ctx, file_name: str):
-    try:
-        # Read the contents of the JSON file
-        with open(SearchFile, 'r') as file:
-            data = json.load(file)
-        # Send the contents of the JSON file as a message
-        await ctx.send(f"Here are all searches:\n```json\n{json.dumps(data, indent=4)}\n```")
-    except FileNotFoundError:
-        # If the file is not found, send an error message
-        await ctx.send(f"File '{file_name}' not found.")
+        await ctx.send(f"{item} added to the list along with your Discord user ID, {user_id}.")
 
-@bot.command()
-async def show_my_list(ctx, file_name: str):
-    try:
-        # Read the contents of the JSON file
-        with open(SearchFile, 'r') as file:
-            data = json.load(file)
-        # Send the contents of the JSON file as a message
-        await ctx.send(f"Here are your searches:\n```json\n{json.dumps(data, indent=4)}\n```")
-    except FileNotFoundError:
-        # If the file is not found, send an error message
-        await ctx.send(f"File '{file_name}' not found.")
+    @commands.command()
+    async def show_whole_list(self, ctx):
+        try:
+            with open(self.SearchFile, 'r') as file:
+                data = json.load(file)
+            await ctx.send(f"Here are all searches:\n```json\n{json.dumps(data, indent=4)}\n```")
+        except FileNotFoundError:
+            await ctx.send("The search list file was not found.")
+        except json.JSONDecodeError:
+            await ctx.send("The search list file is empty or corrupted.")
+
+
+    @commands.command()
+    async def help(self, ctx):
+        help_message = """
+        **Commands:**
+        - `/add_to_list [item]`: Add an item to the search list.
+        - `/show_whole_list`: Show the entire search list.
+        - `/help`: Display this help message.
+        """
+        await ctx.send(help_message)
+
+def setup(bot):
+    bot.add_cog(CommandCog(bot))
--- DiscordDriver.py
+++ DiscordDriver.py
@@ -1,8 +1,7 @@
-from discord import app_commands
-from discord.ext import commands
+from discord.ext import commands, tasks
 from Assets.Utilities import *
 from Assets.VinylScraper import *
-#from Assets.UserCommands import *
+from Assets.UserCommands import *
 import discord 
 import asyncio
 import json
@@ -13,15 +12,28 @@ import json
 
 
 def run_discord_bot():
+    token, URL = Parse_Private()
+    intents = discord.Intents.all()
+    bot = commands.Bot(command_prefix='/', intents=intents)
 
-    ###########Initialize Bot####################################
-    token, URL = Parse_Private()                                #
-    #Set the bot's status                                       #
-    intents = discord.Intents.all()                             #
-    client = commands.Bot(command_prefix='/', intents=intents)  #
-    #############################################################
+    # Load the UserCommands module as an extension
+    
+    
+
+    @bot.event
+    async def on_ready():
+        print(f'We have logged in as {bot.user}')
+        await bot.change_presence(activity=discord.Game("with your mom's record player"))
+        
+    bot.load_extension('Assets.UserCommands')
+    bot.run(token)
+
+if __name__ == "__main__":
+    run_discord_bot()
 
 
+#Commented Out for Testing Purposes
+##########################################################################################################
 model  = "gpt-3.5-turbo-0125"
 #GPT_Prompt = "Given a list of subreddit post titles about vinyl album releases, your task is to analyze these titles, which are user-generated and may contain duplicates or different wordings for the same release. Your objective is to identify unique releases, disregard exact duplicates and near-duplicates, and output a processed list with unique album releases. Consider variations in color as unique and include them in the processed list. Use your understanding of natural language to interpret varied expressions and terminologies users might use to describe the same album. Occasionally you will be given an empty list. In this case, please output 'No new releases found.'"
 Duplicate_Removal_Prompt = """
@@ -46,47 +58,19 @@ paying close attention to the details of each entry, as there may be slight vari
 Use your understanding of artist names and album titles to make accurate comparisons even when faced with minor differences in spelling or formatting.
 """
 
-
-
 #Dupes_Removed = RemoveDuplicates(get_recent_posts(80), Duplicate_Removal_Prompt, model)
 #print(Dupes_Removed)
 #
 
-
-
 # Load the JSON data from 'SearchParms.json'
-with open('Assets/SearchParams.json', 'r') as file:
-    data = json.load(file)
+# with open('Assets/SearchParams.json', 'r') as file:
+#     data = json.load(file)
 
-all_artists = []
-for user_likes in data.values():
-    all_artists.extend(user_likes)
+# all_artists = []
+# for user_likes in data.values():
+#     all_artists.extend(user_likes)
 
-# The 'all_artists' list now contains all the artist names
-print(json.dumps(all_artists, indent=2))
-
-
-#Commented Out for Testing Purposes
-##########################################################################################################
-#     @client.event
-#     async def on_ready():
-#             #Variable Initialization
-#             #####################################################################
-#             print('We have logged in as {0.user}'.format(client))
-#             await client.change_presence(activity=discord.Game("with your moms's record player"))
-
-            
-#             while True:
-#                 recent_posts = get_recent_posts(50)
-#                 print(recent_posts)
+# # The 'all_artists' list now contains all the artist names
+# print(json.dumps(all_artists, indent=2))
 
 
-
-
-
-#                 await asyncio.sleep(1800)
-
-
-
-#     client.run(token) 
-# run_discord_bot()  
